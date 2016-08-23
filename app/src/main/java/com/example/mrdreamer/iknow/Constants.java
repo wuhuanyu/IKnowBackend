de2c65f3179bb1477ffaf1f5491c201cb63d4a3b
wuhuanyu <1790949749@qq.com>
2016-08-23 10:59:21 +0800

    First

--- app/src/main/java/com/example/mrdreamer/iknow/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.example.mrdreamer.iknow;
-
-/**
- * Created by stack on 8/21/16.
- */
-public class Constants {
-    public static final String PROTOCOL="http://";
-    public static final String  SERVER="192.168.1.103";
-}
